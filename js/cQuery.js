b924fa19e37fd9ec94fc1656fff7512bda891029
cjshinning <784319417@qq.com>
2020-04-21 16:18:55 +0800

    整体架构-常规操作

---
+++ js/cQuery.js
@@ -0,0 +1,15 @@
+var cQuery = function(selector, context){
+    // 构造函数
+}
+
+cQuery.prototype = {
+    // 原型
+    name: function(){
+        console.log(111)
+    },
+    age: function(){}
+}
+
+var a = new cQuery();
+
+a.name();
