0d1ad5f862050b0c21f746c1445fa6f151c2b0b9
cjshinning <784319417@qq.com>
2020-04-21 16:50:24 +0800

    1、整体架构-分割作用域处理

--- js/cQuery.js
+++ js/cQuery.js
@@ -1,13 +1,17 @@
 var cQuery = function(selector, context){
-    return cQuery.prototype.init();
+    return new cQuery.prototype.init();
 }
 
 cQuery.prototype = {
     init: function(){
         return this;
     },
-    name: function(){},
-    age: function(){}
+    name: function(){
+        return this.age
+    },
+    age: 20
 }
 
-console.log(cQuery())
+cQuery.prototype.init.prototype = cQuery.prototype;
+
+console.log(cQuery().name())
