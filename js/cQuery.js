779d298c1b96ab81071214eca15ae70fd91e26c6
cjshinning <784319417@qq.com>
2020-04-22 10:46:11 +0800

    2、源码分析core-选择器

--- js/cQuery.js
+++ js/cQuery.js
@@ -1,10 +1,35 @@
 var cQuery = function(selector, context){
-    return new cQuery.prototype.init();
+    return new cQuery.prototype.init(selector, context);
 }
 
 cQuery.prototype = {
-    init: function(){
-        return this;
+    init: function(selector, context, rootjQuery){
+        if(!selector){
+            return this;
+        }
+        if(typeof selector === 'string'){
+            // 处理id
+            var rquickExpr = /^(?:\s*(<[\w\W]+>)[^>]*|#([\w-]*))$/;
+            var match = rquickExpr.exec(selector);
+            elem = document.getElementById( match[2] );
+            if(elem && elem.parentNode){
+                this.length = 1;
+                this[0] = elem;
+            }
+            this.context = document;
+            this.selector = selector;
+            return this;
+        }else if(selector.nodeType){
+
+        }else if(cQuery.isFunction(selector)){
+            return rootjQuery.ready(selector);
+        }
+
+        if(selector.selector !== undefined){
+            this.selector = selector.selector;
+            this.context = selector.context;
+        }
+        // return cQuery.makeArray(selector, this);
     },
     name: function(){
         return this
@@ -12,10 +37,10 @@ cQuery.prototype = {
     age: 20
 }
 
-cQuery.extend = cQuery.fn.extend = function(){
+// cQuery.extend = cQuery.fn.extend = function(){
     
-}
+// }
 
 cQuery.prototype.init.prototype = cQuery.prototype;
 
-console.log(cQuery().name())
+cQuery('#app');
